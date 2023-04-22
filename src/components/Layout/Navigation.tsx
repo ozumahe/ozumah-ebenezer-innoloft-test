7e7addbe9a8080fe807d8fcab6b90a6670db3938
Ozumah Ebenezer <ozumahe@gmail.com>
2023-04-22 21:16:59 +0100

    Main Page Main

--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -11,21 +11,23 @@ const Navigation: FC = () => {
 
   return (
     <div className="hidden lg:block absolute left-0 top-0 w-[300px] p-[20px]">
-      <div className="flex items-center gap-3">
-        <img
-          src={user.profilePicture}
-          alt={user.firstName}
-          className="w-[60px] h-[60px] rounded-full object-cover"
-        />
-        <div>
-          <p className="text-[14px] font-semibold text-[#6B7280]">
-            {user.firstName} {user.lastName}
-          </p>
-          <p className="text-[14px] font-normal text-[#6B7280] mt-[5px]">
-            {company.name}
-          </p>
+      {user.profilePicture ? (
+        <div className="flex items-center gap-3">
+          <img
+            src={user.profilePicture}
+            alt={user.firstName}
+            className="w-[60px] h-[60px] rounded-full object-cover"
+          />
+          <div>
+            <p className="text-[14px] font-semibold text-[#6B7280]">
+              {user.firstName} {user.lastName}
+            </p>
+            <p className="text-[14px] font-normal text-[#6B7280] mt-[5px]">
+              {company.name}
+            </p>
+          </div>
         </div>
-      </div>
+      ) : null}
       <div className="">
         <a href="/">
           <div className="mt-[30px] flex gap-[20px] cursor-pointer">
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Main: FC = () => {
   return (
-    <div className="w-full p-[20px] flex justify-center items-center">
+    <div className="w-full h-[70vh] p-[20px] flex justify-center items-center">
       <img src="/imgs/Logogroup.svg" alt="logo" />
     </div>
   );
